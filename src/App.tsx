c5db9aa6adda9d75edc0a75e7e05643c844828af
guohaozhong <1033682873@qq.com>
2021-07-21 16:18:44 +0800

    提交代码

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native'
+export default class App extends Component {
+    render() {
+        return (
+           <View>
+               <Text>ReactNative-ts模板</Text>
+           </View>
+        )
+    }
+}
+const styles=StyleSheet.create({
+    
+})
